68eebc4bb921e0f56691b4405404d66a431e2ef0
Tobias Weber <tobiweber87@gmail.com>
2023-09-21 16:10:24 +0200

    Add hover animation for sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div class="flex flex-col lg:flex-row h-full">
-   <div class="w-full bg-secondaryLight h-1/2 lg:h-full">
+   <div class="w-full lg:hover:w-[110%] bg-secondaryLight h-1/2 lg:h-full transition-all ease-in-out duration-700">
       <PortfolioSection/>
    </div>
-   <div class="w-full bg-secondary h-1/2 lg:h-full">
+   <div class="w-full lg:hover:w-[110%] bg-secondary h-1/2 lg:h-full transition-all ease-in-out duration-700">
       <ContactSection/>
    </div>
 </div>
