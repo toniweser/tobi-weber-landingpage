8bebc552a00b48e9e857b247ffbd439100c2c726
Tobias Weber <tobiweber87@gmail.com>
2026-02-17 23:06:11 +0100

    Add click outside handler to close button in ContactSection

--- src/lib/components/ContactSection.svelte
+++ src/lib/components/ContactSection.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
    import SendIcon from "../icons/SendIcon.svelte";
+   import { onMount } from "svelte";
 
    let open = $state(false);
+   let buttonEl: HTMLButtonElement;
+
+   onMount(() => {
+      const handleClick = (e: MouseEvent) => {
+         if (open && !buttonEl.contains(e.target as Node)) {
+            open = false;
+         }
+      };
+      document.addEventListener("click", handleClick);
+      return () => document.removeEventListener("click", handleClick);
+   });
 </script>
 
 <div class="flex h-full items-center justify-center" data-aos="fade-in" data-aos-delay="350" data-aos-duration="1000">
@@ -18,6 +30,7 @@
    </div>
 </div>
 <button
+   bind:this={buttonEl}
    type="button"
    class="fixed bottom-0 right-0 px-6 pb-4 text-left hover:translate-y-0 transition-all ease-in-out duration-700 translate-y-[100px] text-xs md:text-sm md:translate-y-[120px] lg:text-lg lg:translate-y-[140px] cursor-pointer"
    class:!translate-y-0={open}
